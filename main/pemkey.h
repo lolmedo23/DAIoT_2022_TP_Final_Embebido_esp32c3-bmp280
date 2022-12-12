ad52c247124ce66d48978c1168aa038aea9d980c
lolmedo23 <olmedolucas@gmail.com>
2022-12-12 16:09:25 -0300

    Agregando codigo con lectura de temperatura simulada y envio por mqtt

---
+++ main/pemkey.h
@@ -0,0 +1,32 @@
+/*
+ * pemkey.h
+ *
+ *  Created on: Jul 16, 2020
+ *      Author: leopoldo
+ */
+
+#ifndef MAIN_PEMKEY_H_
+#define MAIN_PEMKEY_H_
+
+/*
+ * Clave PRIVADA del dispositivo registrado en Google Cloud Platform - IotCore
+ * https://cloud.google.com/iot/docs/how-tos/credentials/keys
+ *
+ * Crear claves publica y privada para cada dispositivo desde la consola de GCP con la herramienta OPENSSL
+ * 
+ * openssl req -x509 -nodes -newkey rsa:2048 -keyout rsa_lz_private.pem -out rsa_lz_cert.pem -subj "/CN=unused" -days 36500 
+ *  
+ * rsa_private.pem: The private key that must be securely stored on the device and used to sign the authentication JWT.
+ * rsa_public.pem: The public key that must be stored in Cloud IoT Core and used to verify the signature of the authentication JWT.
+ */
+
+const char GCP_PEM_KEY[] =
+
+"-----BEGIN PRIVATE KEY-----\n"
+"MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDEJcA+5wywwGuk\n"
+-Reemplazar con el certificado-
+"PaZ5aWvzxTMVtSnQg6cHGVREQzrEkFhipW14JiJNwK23lmyleUO/AIWz0+GNXht4\n"
+"EVQYXD0LcJF7OB3v9WGuIxq3\n"
+"-----END PRIVATE KEY-----\n";
+
+#endif /* MAIN_PEMKEY_H_ */
